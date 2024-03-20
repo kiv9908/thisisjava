5865ed951cc37dd9b2cc3855e37798d23a4bb5e9
kiv99 <kiv99@192.168.219.109>
2024-03-21 02:37:25 +0900

    230321 지옥의 복습

---
+++ thisisjava/src/test2/SolTest.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class SolTest {
+
+	public static void main(String[] args) {
+
+		Solution a = new Solution();
+
+		int[][] b = { { 2, 3 }, { 1, 7 }, { 2, 4 }, { 3, 5 } };
+		;
+
+		System.out.println(a.solution(10, b));
+
+	}
+}
--- thisisjava/src/test2/Solution.java
+++ thisisjava/src/test2/Solution.java
@@ -1,51 +1,33 @@
-import java.util.Scanner;
+package test2;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
-
-	// 필드
-	private int tickets;
-	private int[][] requests;
-
-	int grade[][];
-
-	// 생성자
-
 	public int solution(int tickets, int[][] requests) {
 
-		this.tickets = tickets;
-		this.requests = requests;
+		int soldTickets = 0;
 
-	}
+		Arrays.sort(requests, new Comparator<int[]>() {
 
-	int cal() {
-
-		grade = new int[requests.length][2];
-		int maxGrade = 0;
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
 
 		for (int i = 0; i < requests.length; i++) {
 
-			if (requests[i][0] < maxGrade) {
-				maxGrade = requests[i][0];
+			if (tickets >= requests[i][1]) {
+
+				soldTickets += requests[i][1];
+				tickets -= requests[i][1];
 
 			}
 
 		}
 
-		Scanner sc = new Scanner(System.in);
-
-		/*
-		 * 입력 티켓수 int 배열 int[]
-		 * 
-		 * 처리 필드 생성
-		 * 
-		 * 제일 높은 등급 순으로 정렬 if 남은티켓수 < 차감하려는 티켓 수 result = 남은 티켓수 return result else
-		 * 남은티켓수-차감하려는 티켓 수 return result
-		 * 
-		 * 결과 int result = new int();
-		 */
-
-		int soldTickets = 0;
-
 		return soldTickets;
 	}
+
 }
